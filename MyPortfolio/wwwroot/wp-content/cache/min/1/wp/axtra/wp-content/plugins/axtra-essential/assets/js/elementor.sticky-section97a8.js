a62212cb4ddf058f0f8669f721d71709479fcf1a
abhi <gg.abhishekdhakal@gmail.com>
2024-03-17 04:00:15 +0545

    Add project files.

---
+++ MyPortfolio/wwwroot/wp-content/cache/min/1/wp/axtra/wp-content/plugins/axtra-essential/assets/js/elementor.sticky-section97a8.js
@@ -0,0 +1,12 @@
+(function($){var Wcf_Sticky_Menu={elementorSection:function($scope){var $_target=$scope,instance=null,editMode=Boolean(elementorFrontend.isEditMode());instance=new Wcf_Sticky_Menu_Plugin($_target);instance.init(instance)},};Wcf_Sticky_Menu_Plugin=function($target){var self=this,sectionId=$target.data('id'),settings=!1,editMode=Boolean(elementorFrontend.isEditMode()),$window=$(window),$body=$('body');self.init=function(){self.wcf_infos_sticky(sectionId);return!1};self.wcf_infos_sticky=function(sectionId){var wcf_global_sticky=!1;var wcf_sticky_offset=150;var wcf_sticky_type=null;wcf_global_sticky=self.getSettings(sectionId,'wcf_global_sticky');wcf_sticky_type=self.getSettings(sectionId,'wcf_sticky_type');wcf_sticky_offset=parseInt(self.getSettings(sectionId,'wcf_sticky_offset'));if(isNaN(wcf_sticky_offset)){wcf_sticky_offset=150}
+if(wcf_sticky_offset<5){wcf_sticky_offset=110}
+if(wcf_global_sticky=='yes'){$target.addClass('wcf-sticky-container');if(wcf_sticky_type=='top'){$target.addClass('top');$target.removeClass('bottom')}
+if(wcf_sticky_type=='bottom'){$target.addClass('bottom');$target.removeClass('top')}
+if(wcf_sticky_type==''){$target.removeClass('top');$target.removeClass('bottom')}
+$window.on('scroll',function(event){var scroll=$window.scrollTop();if(scroll<wcf_sticky_offset){$target.removeClass("wcf-is-sticky")}else{$target.addClass("wcf-is-sticky")}})}else{$target.removeClass('wcf-sticky-container')}};self.getSettings=function(sectionId,key){var editorElements=null,sectionData={};if(!editMode){sectionId='section'+sectionId;if(!window.wcf_section_sticky_data||!window.wcf_section_sticky_data.hasOwnProperty(sectionId)){return!1}
+if(!window.wcf_section_sticky_data[sectionId].hasOwnProperty(key)){return!1}
+return window.wcf_section_sticky_data[sectionId][key]}else{if(!window.elementor.hasOwnProperty('elements')){return!1}
+editorElements=window.elementor.elements;if(!editorElements.models){return!1}
+$.each(editorElements.models,function(index,obj){if(sectionId==obj.id){sectionData=obj.attributes.settings.attributes}});if(!sectionData.hasOwnProperty(key)){return!1}}
+return sectionData[key]}}
+$(window).on('elementor/frontend/init',function(){elementorFrontend.hooks.addAction('frontend/element_ready/section',Wcf_Sticky_Menu.elementorSection);elementorFrontend.hooks.addAction('frontend/element_ready/container',Wcf_Sticky_Menu.elementorSection)})})(jQuery)
